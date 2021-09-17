80f4980870da80d1e35032de59869011449a878f
mostafij163 <mostafijurrahman163@gmail.com>
2021-09-18 00:07:50 +0600

    test

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,21 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { LoginDTO } from "./dtos/login.dto";
-import { SignUpDTO } from "./dtos/sign-up.dto";
-import { UserService } from "./services/user.service";
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { LoginDTO } from './dtos/login.dto';
+import { SignUpDTO } from './dtos/sign-up.dto';
+import { UserService } from './services/user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private userService: UserService){}
+  constructor(private userService: UserService) {}
 
-    @Post('sign-up')
-    async signUp(@Body() data: SignUpDTO) {
-        return await this.userService.createUser(data)
-    }
+  @Post('sign-up')
+  async signUp(@Body() data: SignUpDTO) {
+    return await this.userService.createUser(data);
+  }
 
     @Post('login')
-    async login(@Body() data: LoginDTO) {
-        return await this.userService.login(data)
-    }
+      @HttpCode(200)
+  async login(@Body() data: LoginDTO) {
+    return await this.userService.login(data);
+  }
 }
